ee9e877f9b0b741712344e2cb138b20359a2be9c
fhero2030 <jcarloasilo@gmail.com>
2023-02-12 22:54:20 +0800

    protect event to without profile

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,6 +1,6 @@
-import { type NextPage } from "next";
+import { type NextPage, type GetServerSideProps } from "next";
 
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 
 import React from "react";
 import ScreenContainer from "../../layouts/ScreenContainer";
@@ -118,3 +118,29 @@ const Edit: NextPage = () => {
 };
 
 export default Edit;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  if (!session.user.profileId) {
+    return {
+      redirect: {
+        destination: "/profile/setup",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
