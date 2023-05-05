d4ab74b763964c54a567f4175e11a59a8351e7bf
fhero2030 <jcarloasilo@gmail.com>
2023-05-05 08:27:32 +0800

    add google ad sense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -6,6 +7,11 @@ export default function Document() {
       <Head>
         <meta name="google" content="notranslate" />
         <title>Hataw Takbo Bataan</title>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3426016467753150"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
