9dbeb636d2b99d1f18369b38910a8da1e6d5d0c0
fhero2030 <jcarloasilo@gmail.com>
2023-10-02 20:56:44 +0800

    autoclose to 2000

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -103,7 +103,7 @@ const Edit: NextPage = () => {
                   numOfParticipants={_count.eventParticipant + reserve}
                   scheduleTime={scheduleTime}
                   closeRegistration={
-                    closeRegistration || _count.eventParticipant >= 3000
+                    closeRegistration || _count.eventParticipant >= 2000
                   }
                   ongoing={!!(timeStart10km || timeStart5km || timeStart3km)}
                   ended={
